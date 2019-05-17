3193fa5395b288f95261737355d1f15b9a291a43
Markus Konkol <mkonkol@arcor.de>
2019-05-17 10:51:55 +0200

    show login information on startpage

--- ui/src/components/app/App.js
+++ ui/src/components/app/App.js
@@ -7,20 +7,26 @@ import { Route, NavLink, HashRouter } from "react-router-dom";
 
 import './App.css';
 import logo from '../../assets/img/o2r-logo-only-white.svg';
-import Privacy from '../footerLinks/Privacy';
-import Impressum from '../footerLinks/Impressum';
+import Privacy from './footerLinks/Privacy';
+import Impressum from './footerLinks/Impressum';
 import Upload from '../upload/Upload';
 import httpRequests from '../../helpers/httpRequests'
 
-const Header = () => {
+const Header = (props) => {
   return (        
     <AppBar id="header">
       <Toolbar>
         <Typography variant="h6" color="inherit" style={{ flex: 1 }}>
           <a href="/"><img src={logo} alt="" id="headerLogo"/></a>
         </Typography>
-        <Button color="inherit">Discover ERC</Button>
-        <Button color="inherit" href="api/v1/auth/login">Login</Button>
+        <Button color="inherit">
+          Discover ERC
+        </Button>
+        {props.loggedIn ? <Button color="inherit">{props.userName} {props.userOrcid}</Button> : ''}
+        <Button color="inherit"
+          href= {props.loggedIn ? "api/v1/auth/logout" : "api/v1/auth/login"} 
+          onClick={() => props.login()}>{props.loggedIn ? 'Logout' : 'Login'}
+        </Button>
         <Button color="inherit">Help</Button>
       </Toolbar> 
     </AppBar>
@@ -45,24 +51,23 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      loggedIn: this.user(),
       userName: null,
       userOrcid: null,
     }
-
-    this.user = this.user.bind(this)
   }
 
   user() {
-    console.log("request")
     httpRequests.getUser()
     .then(
       response => this.setState({
+        loggedIn: true,
         userName: response.data.name,
         userOrcid:response.data.orcid
       }),
     )
     .catch(response => {
-      console.log(response)
+      console.log(response);
     });  
   }
 
@@ -70,12 +75,16 @@ class App extends Component {
   render() {
     return (
       <div id="pageContainer">
-      <Header></Header>
+      <Header 
+        loggedIn = {this.state.loggedIn} 
+        login = {() => this.user()}
+        userName = {this.state.userName}
+        userOrcid = {this.state.userOrcid}>
+      </Header>
       <Footer></Footer>
       <HashRouter>
       <div>
         <div className="content" id="mainView">
-          <button className='button' onClick={() => this.user()}>Click Me</button>
           <Route exact path="/" component={Upload}/>
           <Route path="/impressum" component={Impressum}/>
           <Route path="/privacy" component={Privacy}/>
@@ -92,9 +101,10 @@ export default App
 class Main extends React.Component {
 
   render() {
-    
     return (
-      <div>{this.props.view}</div>
+      <div>
+        {this.props.view}
+      </div>
     );
   }
 }
