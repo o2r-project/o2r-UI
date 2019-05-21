d1371696ec60dff43b7d9b33b7a119dabbb9470b
Markus Konkol <mkonkol@arcor.de>
2019-05-21 17:38:59 +0200

    extract erc examples into json

--- ui/src/components/uploadERC/Examples/Examples.js
+++ ui/src/components/uploadERC/Examples/Examples.js
@@ -2,32 +2,9 @@ import React, { Component } from 'react';
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 
 import './examples.css';
+import ercs from './ercs.json';
 
 function exampleERCs(props, close){
-    const ercs = [{
-        name: "R Markdown",
-        url: "https://uni-muenster.sciebo.de/s/hAh8AZLYvHNgNA9",
-        folder: "/workspace-rmd-data"
-    },{
-        name: "R Markdown (different data)",
-        url: "https://uni-muenster.sciebo.de/s/hAh8AZLYvHNgNA9",
-        folder: "/workspace-rmd-data-other"
-    },{
-        name: "R Markdown (random plot)",
-        url: "https://uni-muenster.sciebo.de/s/hAh8AZLYvHNgNA9",
-        folder: "/workspace-rmd-data-random"
-    },{
-        name: "A question driven socio-hydrological modeling process",
-        url: "https://uni-muenster.sciebo.de/s/hAh8AZLYvHNgNA9",
-        folder: "/corpus_aquestiondrivenprocess",
-        doi: "https://doi.org/10.5194/hess-20-73-2016"
-    },{
-        name: "spacetime: Spatio-Temporal Data in R",
-        url: "https://uni-muenster.sciebo.de/s/hAh8AZLYvHNgNA9",
-        folder: "/corpus_spacetime.zip",
-        doi: "http://dx.doi.org/10.18637/jss.v051.i07"
-    }];
-    
     const ercItems = ercs.map((erc) => {
         return (
             <div> 
