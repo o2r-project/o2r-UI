99ee58a3e717c0b4599496b029067d32607bce11
NJaku01 <n_jaku01@wwu.de>
2019-06-14 16:05:17 +0200

    select licenses + code refactoring

--- ui/src/components/createERC/requiredMetadata/RequiredMetadata.js
+++ ui/src/components/createERC/requiredMetadata/RequiredMetadata.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import { Card, CardContent, TextField, CardActions, IconButton, Button, MenuItem,  } from "@material-ui/core";
 import {Formik} from 'formik';
 import * as Yup from 'yup';
-import data from '../../../helpers/licenses.json'
+import licensesData from '../../../helpers/licenses.json'
 
 import './requiredMetadata.css';
 
+/** 
 function openForm(){
 
 }
 function updateCard(){
  Yup.object()
 }
+*/
+const textLicense=[];
+const dataLicense=[];
+const codeLicense=[];
 
 function prepareLicense(){
-    console.log(data);
-    //httpRequests.getLicenses().then(function (result){console.log(result)})
+    console.log(licensesData);
+        for(var i in licensesData){
+            if(licensesData[i].domain_content){ textLicense.push(licensesData[i])};
+            if(licensesData[i].domain_data) {dataLicense.push(licensesData[i])};
+            if(licensesData[i].domain_software) {codeLicense.push(licensesData[i])};
+}
+    console.log (textLicense)
 }
 
 
@@ -31,13 +41,16 @@ function Authors(props) {
                 </CardContent>
                 <CardActions disableSpacing>
                     <IconButton aria-Label="Edit"
-                    onClick={openForm}>
+                    //onClick={}
+                    >
                         <i>Edit</i>
                     </IconButton>
                 </CardActions>
                 
                 <Formik>
-                <form onSubmit={updateCard}>
+                <form 
+                //onSubmit={}
+                >
                     <input
                     type='text'
                     name='Name'/>
@@ -53,115 +66,8 @@ function Authors(props) {
     return listItems;
 }
 
-const licenses =[
-    {
-        value: 'AFL-3.0',
-        label: 'Academic Free License 3.0 (AFL-3.0)'
-    },
-    {
-        value: 'Against-DRM',
-        label: 'Against DRM (Against-DRM) '
-    },
-    {
-        value: 'CC-BY-4.0',
-        label: 'Creative Commons Attribution 4.0 (CC-BY-4.0)'
-    },
-    {
-        value: 'CC-BY-NC-4.0',
-        label: 'Creative Commons Attribution-NonCommercial 4.0 (CC-BY-NC-4.0) '
-    },
-    {
-        value: 'CC-BY-SA-4.0',
-        label: 'Creative Commons Attribution Share-Alike 4.0 (CC-BY-SA-4.0)'
-    },
-    {
-        value: 'CC0 1.0',
-        label: 'CC0 1.0 (CC0-1.0)'
-    },
-    {
-        value: 'DSL',
-        label: 'Design Science License (DSL)'
-    },
-    {
-        value: 'FAL-1.3',
-        label: 'Free Art License 1.3 (FAL-1.3)'
-    },
-    {
-        value: 'GFDL-1.3-no-cover-text-no invariant-sections',
-        label: 'GNU Free Documentation License 1.3 with no cover texts and no invariant sections (GFDL-1.3-no-cover-text-no invariant-sections)'
-    },
-    {
-        value: 'MirOS',
-        label: 'MirOS Licence (MirOS)'
-    },
-    {
-        value: 'NPOSL-3.0',
-        label: 'Non-Profit Open Software License 3.0 (NPOSL-3.0)'
-    },
-    {
-        value: 'OGL-Canada-2.0',
-        label: 'Open Government License 2.0 (Canada) (OGL-Canada-2.0)'
-    },
-    {
-        value: 'OGL-UK-1.0',
-        label: 'Open Government Licence 1.0 (United Kingdom) (OGL-UK-1.0)'
-    },
-    {
-        value: 'OGL-UK-2.0',
-        label: 'Open Government Licence 2.0 (United Kingdom) (OGL-UK-2.0)'
-    },
-    {
-        value: 'OGL-UK-3.0',
-        label: 'Open Government Licence 3.0 (United Kingdom) (OGL-UK-3.0)'
-    },
-    {
-        value: 'OSL-3.0',
-        label: 'Open Software License 3.0 (OSL-3.0)'
-    },
-    {
-        value: 'Talis',
-        label: 'Talis Community License (Talis)'
-    },
-    {
-        value: 'hesa-withrights',
-        label: 'Higher Education Statistics Agency Copyright with data.gov.uk rights (hesa-withrights)'
-    },
-    {
-        value: 'localauth-withrights',
-        label: 'Local Authority Copyright with data.gov.uk rights (localauth-withrights)'
-    },
-    {
-        value: 'other.at',
-        label: 'Other (Attribution) (other-at)'
-    },
-    {
-        value: 'other-open',
-        label: ' Other (Open) (other-open) '
-    },
-    {
-        value: 'other-pd',
-        label: 'Other (Public Domain) (other-pd)'
-    },
-    {
-        value: 'ukclickusepsi',
-        label: 'UK Click Use PSI (ukclickusepsi)'
-    },
-    {
-        value: 'ukcrown-withrights',
-        label: 'UK Crown Copyright with data.gov.uk rights (ukcrown-withrights)'
-    },
-    {
-        value: 'ukpsi',
-        label: 'UK PSI Public Sector Information (ukpsi)'
-    },
 
-    
 
-    
-
-    
-    
-]
 const validationSchema= Yup.object({
     title: Yup.string()
         .required('Titel is required'),
@@ -192,13 +98,17 @@ const Form = props =>{
         isValid,
         setFieldTouched
     }=props
-/** 
-  const handleChange2 = name => event =>{
+
+ /**
+  * 
+  * const handleChange2 = name => event =>{
       console.log(props);
-      this.setState{[name]: event.target.value})
-    }*/
+      props.values.{[name]: event.target.value})
+    }
+*/
 
     const change= (name, e) => {
+        console.log(e);
         e.persist();
         console.log(props);
         handleChange(e);
@@ -329,9 +239,49 @@ const Form = props =>{
         margin="normal"
         variant="outlined"
         >
-        {licenses.map(option =>(
-            <MenuItem key={option.value} value={option.value}>
-                {option.label}
+        {dataLicense.map(option =>(
+            <MenuItem key={option.id} value={option}>
+                {option.title}
+                </MenuItem>
+        ))}
+    </TextField>
+    <TextField
+        id="textLicenses"
+        select
+        label="textLicenses"
+        style={{ margin: 8 }}
+        required
+        fullWidth
+        helperText={touched.textLicenses ? errors.textLicenses : ""}
+        error={touched.textLicenses && Boolean(errors.textLicenses)}
+        value={textLicenses}
+        onChange={change.bind(null, "textLicenses")}
+        margin="normal"
+        variant="outlined"
+        >
+        {textLicense.map(option =>(
+            <MenuItem key={option.id} value={option}>
+                {option.title}
+                </MenuItem>
+        ))}
+    </TextField>
+    <TextField
+        id="codeLicenses"
+        select
+        label="codeLicenses"
+        style={{ margin: 8 }}
+        required
+        fullWidth
+        helperText={touched.codeLicenses ? errors.codeLicenses : ""}
+        error={touched.codeLicenses && Boolean(errors.codeLicenses)}
+        value={codeLicenses}
+        onChange={change.bind(null, "codeLicenses")}
+        margin="normal"
+        variant="outlined"
+        >
+        {codeLicense.map(option =>(
+            <MenuItem key={option.id} value={option}>
+                {option.title}
                 </MenuItem>
         ))}
     </TextField>
@@ -381,16 +331,7 @@ class RequiredMetadata extends Component {
                         initialValues ={values}
                         validationSchema = {validationSchema}
                         />
-                    <TextField
-                        id="date"
-                        label="Publication date"
-                        type="date"
-                        //defaultValue="2017-05-24"
-                        //className={classes.textField}
-                        InputLabelProps={{
-                        shrink: true,
-                        }}
-                    />
+                
               { // </Card>
               }
             </div>
