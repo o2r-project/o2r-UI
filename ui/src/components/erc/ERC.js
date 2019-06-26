40ecc3639d01e0c5b6fc7c704c387f93b556d790
Markus Konkol <mkonkol@arcor.de>
2019-06-26 11:52:20 +0200

    add resizable components

--- ui/src/components/erc/ERC.js
+++ ui/src/components/erc/ERC.js
@@ -1,20 +1,55 @@
-import React, { Component } from "react";
+import React from 'react';
+import 'react-reflex/styles.css';
+import './erc.css';
 
-class ERC extends Component {
-    constructor(props){
-        super(props);
-        this.state={
+import {
+  ReflexContainer,
+  ReflexElement,
+  ReflexSplitter
+} from 'react-reflex'
 
-        }
-    }
+/////////////////////////////////////////////////////////
+// Re-Flex Basic vertical layout non-resizable
+//
+/////////////////////////////////////////////////////////
+class ERC
+  extends React.Component {
+    constructor() {
+        super();
+        this.state = { 
+            
+        };
+      }
+  render () {
 
-    render() {
-        return (
-            <div>
-                <div>ERC</div>
-            </div>
-        );
-    };
+    return (
+        <div className="Erc">
+        <ReflexContainer style={{ height: "87vh" }} orientation="vertical">
+            <ReflexElement className="left-pane">
+                <div className="pane-content">
+                    <label>Left Pane (resizable)</label>
+                </div>
+            </ReflexElement>
+            <ReflexSplitter propagate={true} style={{ width: "10px" }} />
+            <ReflexElement className="right-pane">
+                <ReflexContainer orientation="horizontal">
+                    <ReflexElement className="right-up">
+                        <div className="pane-content">
+                            <label>Right Pane (resizable)</label>
+                        </div>
+                    </ReflexElement>
+                    <ReflexSplitter propagate={true} style={{ height: "10px" }} />
+                    <ReflexElement className="right-bottom">
+                        <div className="pane-content">
+                            <label>Bottom Pane</label>
+                        </div>
+                    </ReflexElement>
+                </ReflexContainer>
+            </ReflexElement>
+        </ReflexContainer>
+      </div>
+    )
+  }
 }
 
 export default ERC;
