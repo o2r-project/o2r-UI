1790da2a89df6d20a9795497fe05a95150475d99
NJaku01 <nick1212@gmx.de>
2019-08-08 10:10:39 +0200

    alert when no Figure Found

--- ui/src/components/erc/Inspect/CodeView/CodeView.js
+++ ui/src/components/erc/Inspect/CodeView/CodeView.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import Highlight from 'react-highlight.js';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
 
 import httpRequests from '../../../../helpers/httpRequests';
-import './codeview.css' 
+import './codeview.css'
+
 
 class CodeView extends React.Component {
 
-    handleMouseUp ( e ) {
+
+    handleMouseUp(e) {
+        var self = this;
+        console.log(e)
+        var target= e.currentTarget
         try {
             httpRequests.searchBinding(window.getSelection().getRangeAt(0).toString(), this.props.metadata)
-            .then(function(res){
-               console.log(res)
-               
-               var answerText= "To manipulate this codeline please check ";
-               for (var i = 0; i < res.data.data.length -1; i++){
-                  answerText += res.data.data[i] + " or "
-               }
-               answerText += res.data.data[res.data.data.length-1] + " in the \"Manipulate\" view"
-               alert(answerText);
-            })
-            .catch(function(res){
-                console.log(res);
-            })
-          } catch (error) {   
+                .then(function (res) {
+                    console.log(res)
+
+                    var answerText
+                    if (res.data.data.length != 0) {
+                        var answerText = "To manipulate this codeline please check ";
+                        for (var i = 0; i < res.data.data.length - 1; i++) {
+                            answerText += res.data.data[i] + " or "
+                        }
+                        answerText += res.data.data[res.data.data.length - 1] + " in the \"Manipulate\" view"
+
+                    } else {
+                        answerText = "Sadly, there is no Figure to Manipulate for this code"
+                    }
+                    alert(answerText);
+                })
+                .catch(function (res) {
+                    console.log(res);
+                })
+        } catch (error) {
         }
     }
 
-    render () {
+    render() {
+        const open = Boolean(this.state.anchorEl);
+        const id = open ? 'simple-popover' : undefined;
         return (
             <div onMouseUp={this.handleMouseUp.bind(this)}>
                 <Highlight language="R" className="code">
@@ -34,7 +49,7 @@ class CodeView extends React.Component {
                 </Highlight>
             </div>
         )
-  }
+    }
 }
 
 export default CodeView;
