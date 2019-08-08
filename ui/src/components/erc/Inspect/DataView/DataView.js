b04eca34fb933f32c514c6615319c5eaeaab8355
markuskonk <m_konk01@uni-muenster.de>
2019-08-08 16:56:15 +0200

    rdata not working

--- ui/src/components/erc/Inspect/DataView/DataView.js
+++ ui/src/components/erc/Inspect/DataView/DataView.js
@@ -2,31 +2,20 @@ import React from 'react';
 
 import CSV from './CSV/CSV';
 import JSON from './JSON/JSON';
-import httpRequests from '../../../../helpers/httpRequests';
+
 
 const DataTable = (props) => {
     let dataFormat = null; 
     for(let i=0;i<props.data.data.tree.length;i++){
         if (props.data.data.tree[i].name === props.data.data.datafile) {
-            if ( props.data.data.tree[i].extension === '.rdata' ) {
-                dataFormat = '.rdata';
-                httpRequests.getFile("inspection/Jb54y?file=mjoBestUse.RData")
-                .then( ( res ) => {
-                    console.log(res.data)
-                })
-            }else{
-                dataFormat = props.data.data.tree[i].type
-            }
-
+            dataFormat = props.data.data.tree[i].type
         }
     }
     switch(dataFormat) {
         case 'text/csv':
-            return <CSV csv={props.data.data.data} />
+            return <CSV csv={props.data.data.data} file={props.data.data.datafile}>csv</CSV>
         case 'application/json':
-            return <JSON json={props.data.data.data[0]} />
-        case '.rdata':
-            return <JSON json={props.data.data.data[0]} />
+            return <JSON json={props.data.data.data[0]} file={props.data.data.datafile}>json</JSON>
         default:
             return <div>No data</div>
     }
@@ -35,6 +24,7 @@ const DataTable = (props) => {
 class DataView extends React.Component {
 
     render () {
+        console.log(this.props)
         return (
             <div>
                 {this.props.data.data ? 
