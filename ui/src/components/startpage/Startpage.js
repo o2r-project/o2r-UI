b75a94ff7f49a43efb89e2e5475c13e108754bf1
dwalin93 <p.glahe@gmx.de>
2019-09-26 12:43:39 +0200

    resolved conflicts

--- ui/src/components/startpage/Startpage.js
+++ ui/src/components/startpage/Startpage.js
@@ -15,29 +15,6 @@ class Startpage extends Component {
 
     return (
       <Grid container spacing={10}>
-<<<<<<< HEAD
-        <Grid item xs={12}>
-          <Grid
-            container
-            justify="center"
-            spacing={Number(spacing)}>
-              <Grid item>
-                <Card id="uploadCard1">
-                  <CardContent>
-                      <Upload />
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card id="uploadCard2">    
-                  <CardContent>
-                      <InspectExamples />
-                  </CardContent>
-                </Card>
-              </Grid>
-          </Grid>
-          <p id="moreInfo">Find more information on our project website <a href="https://o2r.info">https://o2r.info</a></p>
-=======
       <Grid item xs={12}>
         <Grid
           container
@@ -57,7 +34,6 @@ class Startpage extends Component {
             </CardContent>
             </Card>
             </Grid>
->>>>>>> dev
         </Grid>
         <p id="moreInfo">Find more information on our project website <a href="https://o2r.info">https://o2r.info</a></p>
       </Grid>
