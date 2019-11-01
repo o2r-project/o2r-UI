c86261443c8abf84321b2183b868b202151de2c6
NJaku01 <nick1212@gmx.de>
2019-11-01 12:31:06 +0100

    Download files that are larger than 5mb

--- ui/src/components/erc/Inspect/DataView/DataView.js
+++ ui/src/components/erc/Inspect/DataView/DataView.js
@@ -3,18 +3,26 @@ import React from 'react';
 import CSV from './CSV/CSV';
 import JSON from './JSON/JSON';
 import RData from './RData/RData';
+import Download from './Download/Download';
 
 const DataTable = (props) => {
     
     let dataFormat = null; 
+    let dataSize =0;
     let data = props.data.data;
     for( let i=0; i<data.tree.length; i++ ) {
         if ( data.tree[i].name === data.datafile ) {
             dataFormat = data.tree[i].type
+            dataSize = data.tree[i].size
+            console.log(dataSize)
         }
         if ( data.datafile.trim().toLowerCase().indexOf('.rdata') !== -1 ) {
             dataFormat = '.rdata';
         }
+    }
+ // 5mb = 5000000 byte
+    if(dataSize > 5000000){
+        return <Download file={data.datafile} id={props.data.id}/>
     }
     
     switch(dataFormat) {
