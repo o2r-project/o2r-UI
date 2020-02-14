74eb2b89a8fe42a8c24841c2e52efcba27d6445f
MarkusKonk <m_konk01@uni-muenster.de>
2020-02-14 09:04:16 +0100

    add instructions to starting page

--- ui/src/components/startpageDemo/Startpage.js
+++ ui/src/components/startpageDemo/Startpage.js
@@ -16,8 +16,24 @@ class Startpage extends Component {
       <div>
         <div style={{width:"50%", marginTop:"5%", marginLeft: "auto", marginRight: "auto"}}>
           <h1>Create your own Execuable Research Compendium (ERC)</h1>
-            <div style={{width:"50%", marginLeft: "auto", marginRight: "auto"}}>
-            <h3>Upload your workspace (.zip) or <a href="https://github.com/o2r-project/erc-examples/tree/master/ERC/Finished">use existing ones</a></h3>
+            <div style={{width:"65%", marginLeft: "auto", marginRight: "auto"}}>
+              <div className="instruction">
+                <b>Step 1: Create an RMarkdown file including your R analysis</b>
+              </div>
+              <div className="instruction">
+                <b>Step 2: Add metadata to your RMarkdown in YML format like 
+                  <a target="_blank" rel="noopener" href="https://github.com/o2r-project/erc-examples/blob/master/ERC/Finished/INSYDE/workspace/main.Rmd">here</a>
+                </b><br/>
+              </div>
+              <div className="instruction">
+                <b>Step 3: Run your RMarkdown to generate the HTML file</b><br/>
+              </div>
+              <div className="instruction">
+                <b>Step 4: Upload your workspace including the code files, data, and the HTML (.zip)</b><br/>
+              </div>
+              <div className="instruction">
+                <b>No workspace at hand? Just upload one of our <a target="_blank" rel="noopener" href="https://github.com/o2r-project/erc-examples/tree/master/ERC/Finished">example ERCs</a></b>
+              </div>
               <Dropzone />
             </div>
         </div>
