e7bf818e30502e982a79cf8b2828de626e2993a6
Daniel Nüst <daniel.nuest@uni-muenster.de>
2020-04-02 18:23:35 +0200

    Fix typo on startpage and bump version

--- ui/src/components/startpageDemo/Startpage.js
+++ ui/src/components/startpageDemo/Startpage.js
@@ -15,18 +15,17 @@ class Startpage extends Component {
     return (
       <div>
         <div style={{width:"50%", marginTop:"5%", marginLeft: "auto", marginRight: "auto"}}>
-          <h1>Create your own Execuable Research Compendium (ERC)</h1>
+          <h1>Create your own Executable Research Compendium (ERC)</h1>
             <div style={{width:"65%", marginLeft: "auto", marginRight: "auto"}}>
               <div className="instruction">
-                <b>Step 1: Create an RMarkdown file including your R analysis</b>
+                <b>Step 1: Create an R Markdown file including your R analysis</b>
               </div>
               <div className="instruction">
-                <b>Step 2: Add metadata to your RMarkdown in YML format like 
-                  <a target="_blank" rel="noopener" href="https://github.com/o2r-project/erc-examples/blob/master/ERC/Finished/INSYDE/workspace/main.Rmd"> here</a>
+                <b>Step 2: Add metadata to your RMarkdown in YAML format like <a target="_blank" rel="noopener" href="https://github.com/o2r-project/erc-examples/blob/master/ERC/Finished/INSYDE/workspace/main.Rmd">here</a>
                 </b><br/>
               </div>
               <div className="instruction">
-                <b>Step 3: Run your RMarkdown to generate the HTML file</b><br/>
+                <b>Step 3: Run your R Markdown to generate the HTML file</b><br/>
               </div>
               <div className="instruction">
                 <b>Step 4: Upload your workspace including the code files, data, and the HTML (.zip)</b><br/>
