88c5bd3ddd02243fc69bacb8e42f95cfe1eee8ff
NJaku01 <n_jaku01@wwu.de>
2020-04-17 10:41:10 +0200

    back click for comparison #125

--- ui/src/components/erc/Check/Comparison/Comparison.js
+++ ui/src/components/erc/Check/Comparison/Comparison.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Button, Dialog, AppBar, Toolbar, Slide, Typography } from "@material-ui/core";
 import logo from '../../../../assets/img/o2r-logo-only-white.svg';
 import Iframe from 'react-iframe';
-
+import { withRouter } from 'react-router-dom';
 import config from '../../../../helpers/config';
 import './comparison.css';
 import * as $ from 'jquery';
@@ -11,20 +11,33 @@ const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-function ComparisonView(props) {
-    const job = props.job;
-    const [open, setOpen] = React.useState(false);
+class Comparison extends Component {
 
-    function handleClickOpen() {
-        setOpen(true);
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            job: this.props.job
+        }
+    };
+
+    componentDidMount() {
+        const self = this;
+        if (this.props.location.search === '?result') {
+            self.setState({ open: true })
+        }
     }
 
-    function handleClose() {
-        setOpen(false);
+    handleClickOpen = () => {
+        this.props.history.push(this.props.location.pathname + '?result')
     }
 
-    function dialogEntered() {
-        if (job.status === 'success') {
+    handleClose = () => {
+        window.history.back();
+    }
+
+    dialogEntered = () => {
+        if (this.state.job.status === 'success') {
             $("#frame1").contents().scroll(function () {
                 $("#frame2").contents().scrollTop($("#frame1").contents().scrollTop());
             });
@@ -49,57 +62,49 @@ function ComparisonView(props) {
     }
 
 
-
-    return (
-        <div>
-            <Button variant="contained" color="primary"
-                disabled={job.status !== 'failure' && job.status !== 'success'}
-                onClick={handleClickOpen}
-                style={{ marginTop: "5%", width: "150px", }}
-            >
-                Show result
-            </Button>
-            <Dialog className="main_block" fullScreen TransitionComponent={Transition}
-                open={open}
-                onClose={handleClose}
-                onEntered={dialogEntered}
-            >
-                <AppBar>
-                    <Toolbar>
-                        <Typography variant="h6" color="inherit" style={{ flex: 1 }}>
-                            <a href="/"><img src={logo} alt="o2r" id="headerLogo" /></a>
-                        </Typography>
-                        <Button color="inherit" onClick={handleClose}>Close</Button>
-                    </Toolbar>
-                </AppBar>
-                {job.status === 'success' ?
-                    <div className="compare_">
-                        <h4 className="title" style={{ marginLeft: "1%" }}> Original results </h4>
-                        <h4 className="title"> Reproduced results </h4>
-                        <Iframe className="display" id={'frame1'} url={config.baseUrl + "job/" + job.id + "/data/display.html"}></Iframe>
-                        <Iframe className="check" id={'frame2'} url={config.baseUrl + "job/" + job.id + "/data/check.html"}></Iframe>
-                    </div> :
-                    <div className="compare_">
-                        <h4 className="title_" > Original results </h4>
-                        <h4 className="title_"> Reproduced results </h4>
-                        <h4 className="title_"> Differences between original and reproduced results </h4>
-                        <Iframe className="display_" id={'frame1'} url={config.baseUrl + "compendium/" + job.compendium_id + "/data/display.html"}></Iframe>
-                        <Iframe className="check_" id={'frame2'} url={config.baseUrl + "job/" + job.id + "/data/display.html"}></Iframe>
-                        <Iframe className="diff" id={'frame3'} url={config.baseUrl + "job/" + job.id + "/data/check.html"}></Iframe>
-                    </div>
-                }
-            </Dialog>
-        </div>
-    );
-}
-
-class Comparison extends Component {
-
     render() {
         return (
-            <ComparisonView job={this.props.job}></ComparisonView>
+            <div>
+                <Button variant="contained" color="primary"
+                    disabled={this.state.job.status !== 'failure' && this.state.job.status !== 'success'}
+                    onClick={this.handleClickOpen}
+                    style={{ marginTop: "5%", width: "150px", }}
+                >
+                    Show result
+            </Button>
+                <Dialog className="main_block" fullScreen TransitionComponent={Transition}
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    onEntered={this.dialogEntered}
+                >
+                    <AppBar>
+                        <Toolbar>
+                            <Typography variant="h6" color="inherit" style={{ flex: 1 }}>
+                                <a href="/"><img src={logo} alt="o2r" id="headerLogo" /></a>
+                            </Typography>
+                            <Button color="inherit" onClick={this.handleClose}>Close</Button>
+                        </Toolbar>
+                    </AppBar>
+                    {this.state.job.status === 'success' ?
+                        <div className="compare_">
+                            <h4 className="title" style={{ marginLeft: "1%" }}> Original results </h4>
+                            <h4 className="title"> Reproduced results </h4>
+                            <Iframe className="display" id={'frame1'} url={config.baseUrl + "job/" + this.state.job.id + "/data/display.html"}></Iframe>
+                            <Iframe className="check" id={'frame2'} url={config.baseUrl + "job/" + this.state.job.id + "/data/check.html"}></Iframe>
+                        </div> :
+                        <div className="compare_">
+                            <h4 className="title_" > Original results </h4>
+                            <h4 className="title_"> Reproduced results </h4>
+                            <h4 className="title_"> Differences between original and reproduced results </h4>
+                            <Iframe className="display_" id={'frame1'} url={config.baseUrl + "compendium/" + this.state.job.compendium_id + "/data/display.html"}></Iframe>
+                            <Iframe className="check_" id={'frame2'} url={config.baseUrl + "job/" + this.state.job.id + "/data/display.html"}></Iframe>
+                            <Iframe className="diff" id={'frame3'} url={config.baseUrl + "job/" + this.state.job.id + "/data/check.html"}></Iframe>
+                        </div>
+                    }
+                </Dialog>
+            </div>
         );
     }
 }
 
-export default Comparison;
+export default withRouter(Comparison);
