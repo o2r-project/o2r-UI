3b18474d684d43d896e37f5e8a9a224508eb7b07
NJaku01 <nick1212@gmx.de>
2021-05-28 11:16:08 +0200

    implement comments https://github.com/o2r-project/ojs-erc-plugin/issues/2

--- ui/public/config.js
+++ ui/public/config.js
@@ -1,6 +1,6 @@
 const config= {
-    "baseUrl": "http://localhost/api/v1/",
-    "ercUrl": "http://localhost/erc/",
+    "baseUrl": "https://o2r.uni-muenster/api/v1/",
+    "ercUrl": "https://o2r-uni-muenster/erc/",
     "title":" | o2r Demoserver",
     "ojsView" : false,
 }
